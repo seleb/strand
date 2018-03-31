69206da6cd11e903bea8c59347e5666341b662c3
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-31 04:27:21 -0400

    setSource

--- runner.js
+++ runner.js
@@ -1,3 +1,5 @@
+import { parsePassages } from "./parser";
+
 const defaultTitle = "DEFAULT";
 export const defaultPassage = {
 	title: defaultTitle,
@@ -6,13 +8,37 @@ export const defaultPassage = {
 };
 
 export default class {
+	/**
+	 *
+	 * @param {string?} source `this.setSource` is called with `source` as a parameter
+	 */
 	constructor(source) {
 		this.history = [];
 		this.currentPassage = null;
 		this._evalInScope = ((script)=>eval(script)).bind(this);
+
+		this.setSource(source);
+	}
 	eval(script=""){
 	}
+
+	/**
+	 * Parses `source` and assigns the result to `this.passages`.
+	 * If no `source` is provided, assigns an empty object instead.
+	 *
+	 * If no passage with the title `DEFAULT` is provided,
+	 * the runner's default is used.
+	 *
+	 * @param {string?} source Source text to parse, or nothing
+	 */
 	setSource(source) {
+		if (source) {
+			this.passages = parsePassages(source);
+		} else {
+			this.passages = {};
+		}
+		this.passages[defaultTitle] =
+			this.passages[defaultTitle] || defaultPassage;
 	}
 	goto(title) {
 	}
