f0dc2de21d8477122f36a649ccafcf1989cb677d
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-31 08:54:03 -0400

    renderer spec

--- __tests__/runner.test.js
+++ __tests__/runner.test.js
@@ -1,33 +1,72 @@
 import Runner, { defaultPassage } from "../runner";
 
+const renderer = {
+	displayPassage: passage => Promise.resolve()
+};
+
 describe("Runner", () => {
 	it("is an instantiable class", () => {
 		expect(typeof Runner).toBe("function");
 		expect(typeof Runner.prototype).toBe("object");
-		expect(new Runner()).toBeInstanceOf(Runner);
+		expect(new Runner({ renderer })).toBeInstanceOf(Runner);
 	});
-	it("`setSource` is called with the provided argument on initialization", () => {
-		const setSourceSpy = jest.spyOn(Runner.prototype, "setSource");
-		new Runner();
-		expect(setSourceSpy).toHaveBeenCalledWith(undefined);
-		new Runner("::title\nbody");
-		expect(setSourceSpy).toHaveBeenCalledWith("::title\nbody");
+	describe("constructor argument", () => {
+		it("`source` parameter is passed to a call to `setSource` on initialization", () => {
+			const setSourceSpy = jest.spyOn(Runner.prototype, "setSource");
+			new Runner({
+				source: "::title\nbody",
+				renderer
+			});
+			expect(setSourceSpy).toHaveBeenCalledWith("::title\nbody");
+		});
+		it("`source` parameter can be undefined", () => {
+			const setSourceSpy = jest.spyOn(Runner.prototype, "setSource");
+			new Runner({ renderer });
+			expect(setSourceSpy).toHaveBeenCalledWith(undefined);
+		});
+		it("`renderer` parameter is stored on runner as `renderer`", () => {
+			new Runner({ renderer });
+		});
+		it("`renderer` parameter with a function `displayPassage` is required", () => {
+			expect(() => new Runner()).toThrow();
+			expect(
+				() =>
+					new Runner({
+						renderer: {}
+					})
+			).toThrow();
+			expect(
+				() =>
+					new Runner({
+						renderer: {
+							displayPassage: "not a function"
+						}
+					})
+			).toThrow();
+			expect(
+				new Runner({
+					renderer: {
+						displayPassage: () => {}
+					}
+				})
+			).toBeDefined();
+		});
 	});
 
 	describe("setSource", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.goto).toBe("function");
 		});
 		it("sets `this.passages` to an object with the default passage if no source is provided", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			r.setSource();
 			expect(r.passages).toEqual({
 				[defaultPassage.title]: defaultPassage
 			});
 		});
 		it("sets `this.passages` to the parsed result of the provided argument + default passage if given", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			r.setSource("::title\nbody");
 			expect(r.passages).toEqual({
 				title: {
@@ -38,7 +77,7 @@ describe("Runner", () => {
 			});
 		});
 		it("doesn't add the default passage if one is already defined in the source text", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			r.setSource(`::${defaultPassage.title}\nbody`);
 			expect(r.passages).toEqual({
 				[defaultPassage.title]: {
@@ -51,21 +90,21 @@ describe("Runner", () => {
 
 	describe("goto", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.goto).toBe("function");
 		});
 		it("returns a promise", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(r.goto()).toBeInstanceOf(Promise);
 		});
 		it("resolves with no value", () => {
 			expect.assertions(1);
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			return expect(r.goto("test")).resolves.toBeUndefined();
 		});
 		it("calls `displayPassage` with target passage", () => {
 			expect.assertions(1);
-			const r = new Runner("::title\nbody");
+			const r = new Runner({ source: "::title\nbody", renderer });
 			const displayPassageSpy = jest.spyOn(r, "displayPassage");
 			return r
 				.goto("title")
@@ -79,18 +118,21 @@ describe("Runner", () => {
 
 	describe("back", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.back).toBe("function");
 		});
 		it("returns a promise", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			const p = r.goto("test");
 			expect(typeof p.then).toBe("function");
 			expect(typeof p.catch).toBe("function");
 		});
 		it("calls `goto` with the top of the history stack", () => {
 			expect.assertions(1);
-			const r = new Runner("::a\n1\n::b\n2\n::c\n3");
+			const r = new Runner({
+				source: "::a\n1\n::b\n2\n::c\n3",
+				renderer
+			});
 			const gotoSpy = jest.spyOn(r, "goto");
 			return r
 				.goto("a")
@@ -104,24 +146,28 @@ describe("Runner", () => {
 		});
 		it("history has been popped after promise completes", () => {
 			expect.assertions(2);
-			const r = new Runner("::a\n1\n::b\n2\n::c\n3");
+			const r = new Runner({
+				source: "::a\n1\n::b\n2\n::c\n3",
+				renderer
+			});
 			return r
 				.goto("a")
 				.then(() => r.goto("b"))
 				.then(() => r.goto("c"))
 				.then(() => {
 					const h = r.history.slice();
-					return r
-						.back()
-						.then(() => {
-							expect(r.history).toEqual(h.slice(0, -1));
-							expect(r.history).toEqual(["a"]);
-						});
+					return r.back().then(() => {
+						expect(r.history).toEqual(h.slice(0, -1));
+						expect(r.history).toEqual(["a"]);
+					});
 				});
 		});
 		it("resolves with title of new current passage", () => {
 			expect.assertions(2);
-			const r = new Runner("::a\n1\n::b\n2\n::c\n3");
+			const r = new Runner({
+				source: "::a\n1\n::b\n2\n::c\n3",
+				renderer
+			});
 			return r
 				.goto("a")
 				.then(() => r.goto("b"))
@@ -136,25 +182,25 @@ describe("Runner", () => {
 		});
 		it("rejects if called when there is no history", () => {
 			expect.assertions(1);
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			return expect(r.back()).rejects.toBeDefined();
 		});
 	});
 
 	describe("eval", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.eval).toBe("function");
 		});
 		it("evaluates the provided script", () => {
 			const fn = (window.fn = jest.fn());
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			const script = "window.fn()";
 			r.eval(script);
 			expect(fn).toHaveBeenCalled();
 		});
 		it("returns the result of provided script", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(r.eval()).toBeUndefined();
 			expect(r.eval("")).toBeUndefined();
 			expect(r.eval("1")).toBe(1);
@@ -164,31 +210,31 @@ describe("Runner", () => {
 			expect(r.eval("(function(){return 1}())")).toBe(1);
 		});
 		it("`this` refers to the runner inside provided script", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(r.eval("this")).toBe(r);
 			r.eval("this.foo='bar'");
 			expect(r.foo).toBe("bar");
 		});
 		it("can access `window` explicitly", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(r.eval("window")).toBe(window);
 		});
 	});
 
 	describe("getPassageWithTitle", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.getPassageWithTitle).toBe("function");
 		});
 		it("returns the passage with the provided title", () => {
-			const r = new Runner("::title\nbody");
+			const r = new Runner({ source: "::title\nbody", renderer });
 			const p = r.getPassageWithTitle("title");
 			expect(p.title).toBe("title");
 			expect(p.body).toBe("body");
 			expect(Object.keys(p)).toContain("passage");
 		});
 		it("returns default passage if target is not found/invalid", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			const p = r.getPassageWithTitle("title");
 			expect(p.title).toBe(defaultPassage.title);
 			expect(p.body).toBe(defaultPassage.body);
@@ -196,7 +242,7 @@ describe("Runner", () => {
 		});
 		it("fails if target and default passage are invalid", () => {
 			expect.assertions(1);
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			r.passages[defaultPassage.title] = null;
 			return expect(() => r.getPassageWithTitle("test")).toThrow();
 		});
@@ -204,16 +250,16 @@ describe("Runner", () => {
 
 	describe("displayPassage", () => {
 		it("is a function", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(typeof r.displayPassage).toBe("function");
 		});
 		it("returns a promise", () => {
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			expect(r.displayPassage()).toBeInstanceOf(Promise);
 		});
 		it("`currentPassage` is provided passage after promise completes", () => {
 			expect.assertions(1);
-			const r = new Runner("::title\nbody");
+			const r = new Runner({ source: "::title\nbody", renderer });
 			const p = r.getPassageWithTitle("title");
 			return r
 				.displayPassage(p)
@@ -221,7 +267,7 @@ describe("Runner", () => {
 		});
 		it("`currentPassage.title` at time of call has been pushed to history after promise completes", () => {
 			expect.assertions(1);
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			r.currentPassage = {
 				title: "history check"
 			};
@@ -231,18 +277,26 @@ describe("Runner", () => {
 		});
 		it("history is not pushed if `currentPassage` is falsey", () => {
 			expect.assertions(1);
-			const r = new Runner();
+			const r = new Runner({ renderer });
 			return r
 				.displayPassage(r.getPassageWithTitle("title"))
 				.then(() => expect(r.history.length).toBe(0));
 		});
 		it("tells renderer to display passage", () => {
-			// TODO
-			throw new Error();
-		});
-		it("resolves when renderer has displayed passage", () => {
-			// TODO
-			throw new Error();
+			expect.assertions(1);
+			const mockDisplayPassage = jest.fn();
+			mockDisplayPassage.mockReturnValue(Promise.resolve());
+			const r = new Runner({
+				renderer: {
+					displayPassage: mockDisplayPassage
+				}
+			});
+			const p = r.getPassageWithTitle();
+			r
+				.displayPassage(p)
+				.then(() =>
+					expect(mockDisplayPassage.mock.calls.length).toBe(1)
+				);
 		});
 	});
 });
