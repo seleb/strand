f0e94ef81ab3cd460bfaa73f14a08cbb2a31b825
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-04-02 11:53:33 -0400

    some cleanup

--- parser.js
+++ parser.js
@@ -49,7 +49,10 @@ export function parsePassage(passage) {
 	if (!passage) {
 		throw new Error("No passage provided");
 	}
-	const { title, body } = passage;
+	const {
+		title,
+		body
+	} = passage;
 	if (!title) {
 		throw new Error("Passage must have a title");
 	}
@@ -62,74 +65,70 @@ export function parsePassage(passage) {
 	};
 }
 
-import { getLexer } from "./lexer";
+import {
+	getLexer
+} from "./lexer";
 import myrules from "./lang";
 
 const lex = getLexer(myrules);
+
 function compile(source) {
 	return parse(lex(source));
 }
+
 function parse(tokens) {
-	console.warn(tokens);
 	const res = [];
 	const stack = [res];
-	// let current = res;
 	for (let token of tokens) {
-		console.log(stack);
-		console.log(token.name);
-		// console.log(res,`
-// `,current);
 		switch (token.name) {
-			case "if": {
-				const condition = {
-					name: "condition",
-					value: [
-						{
+			case "if":
+				{
+					const condition = {
+						name: "condition",
+						value: [{
 							condition: token.value,
 							branch: []
-						}
-					]
-				};
+						}]
+					};
 
-				stack[stack.length - 1].push(condition);
-				stack.push(condition.value); // enter condition
-				stack.push(condition.value[0].branch); // enter condition's branch
-				// current = condition.value[0].branch;
-				break;
-			}
-			case "elseif": {
-				const branch = {
-					condition: token.value,
-					branch: []
-				};
-				stack.pop(); // exit condition's branch
-				stack[stack.length - 1].push(branch); // last entry in stack is now active condition, so add to its branches
-				stack.push(branch.branch); // enter condition's branch
-				// current = branch.branch;
-				break;
-			}
+					stack[stack.length - 1].push(condition);
+					stack.push(condition.value); // enter condition
+					stack.push(condition.value[0].branch); // enter condition's branch
+					break;
+				}
+			case "elseif":
+				{
+					const branch = {
+						condition: token.value,
+						branch: []
+					};
+					stack.pop(); // exit condition's branch
+					stack[stack.length - 1].push(branch); // last entry in stack is now active condition, so add to its branches
+					stack.push(branch.branch); // enter condition's branch
+					break;
+				}
 			case "endif":
 				stack.pop(); // exit condition's branch
 				stack.pop(); // exit condition
-				// current = stack[stack.length - 1];
 				break;
 			case "action":
-			stack[stack.length - 1].push(token);
+				stack[stack.length - 1].push(token);
 				break;
-			case "fill": {
-				// fill nodes are just plain text
-				const n = {
-					...token,
-					name: "text"
-				};
-				stack[stack.length - 1].push(n);
-				break;
-			}
+			case "fill":
+				{
+					// fill nodes are just plain text
+					const n = {
+						...token,
+						name: "text"
+					};
+					stack[stack.length - 1].push(n);
+					break;
+				}
 			default:
 				throw new Error(`Unrecognized token name: ${token.name}`);
 		}
 	}
-	if(stack.length !== 1){
+	if (stack.length !== 1) {
 		throw new Error(`Unclosed stack: ${stack}`);
 	}
 	return res;
