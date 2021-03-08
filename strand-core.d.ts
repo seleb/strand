21096529c963367117c9f6593aee8a0787450d29
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-03-07 19:14:35 -0500

    feat: add rough typescript declarations

---
+++ strand-core.d.ts
@@ -0,0 +1,40 @@
+declare module 'strand-core' {
+	type ProgramNode =
+		| {
+				name: 'text';
+				value: string;
+		  }
+		| {
+				name: 'action';
+				value: {
+					text: string;
+					action: string;
+				};
+		  }
+		| {
+				name: string;
+				value: never;
+		  };
+	class Passage {
+		title: string;
+		body: string;
+		program: unknown;
+	}
+	abstract class Renderer<T = void> {
+		displayPassage(passage: Passage): Promise<T>;
+	}
+	export default class Strand<T = void> {
+		busy: boolean;
+		history: string[];
+		renderer: Renderer<T>;
+		constructor(options: { renderer: Renderer<T>; source: string });
+
+		eval(source: string): unknown;
+		setSource(source: string): void;
+		getPassageWithTitle(title: string): Passage;
+		goto(passage: string): Promise<T>;
+		back(): Promise<T>;
+		displayPassage(passage: Passage): Promise<T>;
+		execute(program: unknown): ProgramNode[];
+	}
+}
